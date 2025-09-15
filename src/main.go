8d576f9e1157960ad613cbe9b4fae08a96573b5c
guiarthur <arthur.guillaume@ynov.com>
2025-09-15 18:28:48 +0200

    ajout menu

--- src/main.go
+++ src/main.go
@@ -1,9 +1,25 @@
 package main
 
-import RED "RED/structure"
+import (
+	RED "RED/src/structure"
+	"fmt"
+)
 
 func main() {
 	perso := RED.InitCharacter("Aragorn", "Ranger", 10, 100, 100, []string{"Arc", "Flèches", "Potion"})
-	RED.DisplayInfo(perso)
-	RED.AccessInventory(perso)
+	for {
+		RED.AfficherMenu()
+		choix := RED.LireChoix()
+		switch choix {
+		case "1":
+			RED.DisplayInfo(perso)
+		case "2":
+			RED.AccessInventory(perso)
+		case "3":
+			fmt.Println("À bientôt !")
+			return
+		default:
+			fmt.Println("Choix invalide.")
+		}
+	}
 }
---
+++ src/structure/menu.go
@@ -0,0 +1,17 @@
+package RED
+
+import "fmt"
+
+func AfficherMenu() {
+	fmt.Println("\n=== MENU ===")
+	fmt.Println("1. Afficher les infos du personnage")
+	fmt.Println("2. Accéder à l'inventaire")
+	fmt.Println("3. Quitter")
+	fmt.Println("Votre choix : ")
+}
+
+func LireChoix() string {
+	var choix string
+	fmt.Scanln(&choix)
+	return choix
+}
