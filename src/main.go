db7f7b65cc35f570ac505c4c8d281ccfa44c45aa
anomenaf <nomenafitia.andry@ynov.com>
2025-09-15 16:26:08 +0200

    struct personnage

--- src/main.go
+++ src/main.go
@@ -1,1 +1,8 @@
 package main
+
+import RED "RED/structure"
+
+func main() {
+	perso := RED.InitCharacter("Aragorn", "Ranger", 10, 100, 100)
+	RED.DisplayInfo(perso)
+}
---
+++ src/structure/personnage.go
@@ -0,0 +1,32 @@
+package RED
+
+import (
+	"fmt"
+)
+
+type Personnage struct {
+	Nom       string
+	Classe    string
+	Niveau    int
+	PvMax     int
+	PvActuels int
+}
+
+func InitCharacter(nom string, classe string, niveau int, pvMax int, pvActuels int) Personnage {
+	return Personnage{
+		Nom:       nom,
+		Classe:    classe,
+		Niveau:    niveau,
+		PvMax:     pvMax,
+		PvActuels: pvActuels,
+	}
+}
+
+func DisplayInfo(perso Personnage) {
+	fmt.Println("=== Informations du personnage ===")
+	fmt.Println("Nom :", perso.Nom)
+	fmt.Println("Classe :", perso.Classe)
+	fmt.Println("Niveau :", perso.Niveau)
+	fmt.Println("PV actuels :", perso.PvActuels)
+	fmt.Println("PV max :", perso.PvMax)
+}
